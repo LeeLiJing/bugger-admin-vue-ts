3b4b1957ccbd985dbec6c5c306ceb084bcd4a83e
lijing <lijingqg@cdeledu.com>
2021-06-18 19:37:56 +0800

    add user views

---
+++ src/modules/base/views/user.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="system-user">
+    user
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+
+export default defineComponent({
+  name: 'SystemUser',
+
+  setup() {
+
+  }
+});
+</script>
