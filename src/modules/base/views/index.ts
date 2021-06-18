77b3f6a329bebf3035e2da6e3c51cbd13878d98a
lijing <lijingqg@cdeledu.com>
2021-06-18 19:37:39 +0800

    add role modules

--- src/modules/base/views/index.ts
+++ src/modules/base/views/index.ts
@@ -3,5 +3,10 @@ export default [
         label: '用户列表',
         path: '/sys/user',
         component: () => import('./user.vue')
+    },
+    {
+        label: '角色列表',
+        path: '/sys/role',
+        component: () => import('./role.vue')
     }
 ];
